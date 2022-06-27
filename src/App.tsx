b25d52662c8df5b11ebc3e5720ab7bc77cce67ca
Aleksandr_UB <bogdanov777000@mail.ru>
2022-06-27 11:11:22 +0300

    1-localStorage до использования useEffect - begin

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState<number>(0)
+    const incHandler = () => {
+        setValue(value + 1)
+    }
+    // устанавливаем значение
+    const setToLocalStorageHandler = () => {
+        // записать значения:
+        localStorage.setItem('counterValue', JSON.stringify(value))
+        localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))
+    }
+// sessionStorage - отличается временем жизни = жизнь сессии то время пока вкладка открыта - использует такие же методы как "локалсторедж":
+    /* const setToLocalStorageHandler = () => {
+         // записать значения:
+         sessionStorage.setItem('counterValue', JSON.stringify(value))
+         sessionStorage.setItem('counterValue + 1', JSON.stringify(value + 1))
+     }*/
+    // получить значение ранее установленное с локал сторедж независимо от того насколько мы теперь изменили счетчик, получаем "засетенное" значение
+    const getToLocalStorageHandler = () => {
+        let valString = localStorage.getItem('counterValue')
+        if (valString) {
+            let newValue = JSON.parse(valString)
+            setValue(newValue)
+        }
+    }
+    // очищение локал...
+    const clearLocalStorage = () => {
+        localStorage.clear()
+        setValue(0)
+    }
+    // removeItem зачищает переданные данные, а clear зачищает весь локал...
+    const removeItem = () => {
+        localStorage.removeItem('counterValue + 1')
+    }
+    // чтобы при загрузке приложения мы СРАЗУ получали локал, а не по нажатию на кнопку getToLocalStorage и так же когда "инкрементим чтобы сразу в Aplication добавлялось значение используем useEffect()":
+
+    return (
+        <div className="App">
+            <h1>{value}</h1>
+            <button onClick={incHandler}>inc</button>
+            <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+            <button onClick={getToLocalStorageHandler}>getToLocalStorage</button>
+            <button onClick={clearLocalStorage}>clear_local_St</button>
+            <button onClick={removeItem}>remove-item</button>
+        </div>
+    );
 }
 
 export default App;
